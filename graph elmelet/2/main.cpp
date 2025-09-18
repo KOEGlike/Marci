9ef0ab966ccb19040fe7143a10f23d4a20202f4c
Programed-by-hunor <burian.hunor@szekelymikokollegium.ro>
2025-09-18 08:14:46 +0300

    fixed

--- graph elmelet/2/main.cpp
+++ graph elmelet/2/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <cstdlib>
 #include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
@@ -65,29 +65,57 @@ public:
             head = new_element;
         }
     }
-
     void beszur_rendez(int adat)
     {
-        if (head == nullptr)
+        if (head == NULL)
         {
             head = tail = new ListElement(adat);
         }
         else
         {
-
             if (adat >= head->adat)
             {
-                head = new ListElement(adat, head);
+                ListElement *tmp = new ListElement(adat);
+                tmp->next = head;
+                head = tmp;
             }
             else
             {
                 ListElement *p;
-                for (p = head; p->next != nullptr && p->next->adat > adat; p = p->next)
+                for (p = head; p->next != NULL && adat < p->next->adat; p = p->next)
                     ;
 
-                p->next = new ListElement(adat, p->next);
+                ListElement *tmp = new ListElement(adat);
+                tmp->next = p->next;
+                p->next = tmp;
             }
         }
+    }
+
+    void rendez()
+    {
+        for (ListElement *pi = this->head; pi != NULL; pi = pi->next)
+        {
+            ListElement *pmin = pi;
+            for (ListElement *pj = pi->next; pj != NULL; pj = pj->next)
+            {
+                if (pj->adat < pmin->adat)
+                {
+                    pmin = pj;
+                }
+            }
+            if (pmin != pi)
+            {
+                swap(pmin->adat, pi->adat);
+            }
+        }
+        /*ListElement *tmp = pmin->next;
+        if(pmin->next!=NULL)
+        {
+            pmin->next = pmin->next->next;
+        }
+        tmp->next = head;
+        head = tmp;*/
     }
 
     void kiir()
@@ -98,62 +126,43 @@ public:
             cout << i->adat << " ";
             i = i->next;
         }
-        cout << endl
-             << endl;
+        cout << endl;
     }
-
-    friend List fesul_maskepp(List &l1, List &l2);
-    friend List fesul(List &l1, List &l2);
+    friend List fesul(List list1, List list2);
 };
 
-void feltolt(List &l, int n, int max)
+void feltolt_rendez(List &l, int n)
 {
     for (int i = 0; i < n; i++)
     {
-        l.beszur_rendez(rand() % max);
+        l.beszur_rendez(rand() % 100);
     }
 }
 
-List fesul(List &l1, List &l2)
+void feltolt_elol(List &l, int n)
 {
-    List l3;
-
-    ListElement *p1 = l1.head;
-    ListElement *p2 = l2.head;
-
-    while (p1 != nullptr && p2 != nullptr)
+    for (int i = 0; i < n; i++)
     {
-        if (p1->adat > p2->adat)
-        {
-            l3.beszur_elol(p1->adat);
-            p2 = p2->next;
-        }
-        else
-        {
-            p1 = p1->next;
-        }
+        l.beszur_elol(rand() % 100);
     }
-    return l3;
 }
 
-List fesul_maskepp(List &l1, List &l2)
+List fesul(List list1, List list2)
 {
-    List l3;
+    List list3;
+    ListElement *p1head = list1.head;
+    ListElement *p2head = list2.head;
+    ListElement *p3head = list3.head;
+    ListElement *p3tail = list3.tail;
 
-    ListElement *p1head = l1.head;
-    ListElement *p2head = l2.head;
-    ListElement *p3tail = l3.tail;
-    ListElement *p3head = l3.head;
-
-    while (p1head != nullptr && p2head != nullptr)
+    while (p1head != NULL && p2head != NULL)
     {
-
         if (p1head->adat > p2head->adat)
         {
             ListElement *tmp = new ListElement(p1head->adat);
-            if (p3head == nullptr)
+            if (p3head == NULL)
             {
-                p3tail = p3head = tmp;
+                p3head = p3tail = tmp;
             }
             else
             {
@@ -165,9 +174,9 @@ List fesul_maskepp(List &l1, List &l2)
         else
         {
             ListElement *tmp = new ListElement(p2head->adat);
-            if (p3head == nullptr)
+            if (p3head == NULL)
             {
-                p3tail = p3head = tmp;
+                p3head = p3tail = tmp;
             }
             else
             {
@@ -177,21 +186,37 @@ List fesul_maskepp(List &l1, List &l2)
             p2head = p2head->next;
         }
     }
-
-    l3.head = p3head;
-    l3.tail = p3tail;
-    return l3;
+    while (p1head != NULL)
+    {
+        p3tail->next = new ListElement(p1head->adat);
+        p3tail = p3tail->next;
+        p1head = p1head->next;
+    }
+    while (p2head != NULL)
+    {
+        p3tail->next = new ListElement(p2head->adat);
+        p3tail = p3tail->next;
+        p2head = p2head->next;
+    }
+    list3.head = p3head;
+    list3.tail = p3tail;
+    return list3;
 }
 
 int main()
 {
     srand(time(0));
-    int n = 5;
+    int n = 20;
     List l1 = List(), l2 = List();
-    feltolt(l1, n, 100);
-    feltolt(l2, n, 100);
+    feltolt_rendez(l1, n);
     l1.kiir();
+    feltolt_rendez(l2, n);
     l2.kiir();
-    List l3 = fesul_maskepp(l1, l2);
-    l3.kiir();
+    List list3 = fesul(l1, l2);
+    list3.kiir();
+    List list4;
+    feltolt_elol(list4, n);
+    list4.kiir();
+    list4.rendez();
+    list4.kiir();
 }
