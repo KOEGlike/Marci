c5089a63a49da22d23fffae1f71e2641d2e49ad7
Programed-by-hunor <burian.hunor@szekelymikokollegium.ro>
2025-09-24 08:46:26 +0300

    added dfs

--- Graph/DFS/main.cpp
+++ Graph/DFS/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <stack>
+#include <vector>
 
 #define N 50
 
@@ -15,6 +17,8 @@ int beOlvas(string fileName, int graf[][N])
     {
         int x, y;
         f >> x >> y;
+        x--;
+        y--;
         graf[x][y] = 1;
         graf[y][x] = 1;
     }
@@ -34,7 +38,31 @@ void kiIr(int n, int graf[][N])
     }
 }
 
-int dfs()
+void dfs(int n, int graf[][N], int start)
+{
+    stack<int> verem;
+    bool visited[n] = {0};
+    verem.push(start);
+    while (!verem.empty())
+    {
+        int u = verem.top();
+        verem.pop();
+        if (!visited[u])
+        {
+            visited[u] = true;
+            cout << u << " ";
+            for (int i = 0; i < n; i++)
+            {
+                if (!visited[i] && graf[u][i])
+                {
+                    verem.push(i);
+                }
+            }
+        }
+    }
+}
+
+void bfs(int n, int graf[][N], int start)
 {
 }
 
@@ -43,5 +71,6 @@ int main()
     int graf[N][N] = {0};
     int csSz = beOlvas("C:\\Users\\Elev\\Documents\\XI.A\\Marci\\Graph\\DFS\\graf.txt", graf);
     kiIr(csSz, graf);
+    dfs(csSz, graf, 0);
     cout << csSz;
 }
